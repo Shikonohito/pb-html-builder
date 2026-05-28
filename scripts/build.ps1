ca35b1694ccbaae2e4e7fce7330e5d0192aa41e0
Shikonohito <shikonohito@gmail.com>
2026-05-28 11:30:05 +0400

    Remove all scripts.

--- scripts/build.ps1
+++
@@ -1,1 +0,0 @@
-dotnet build ../src/PbHtmlEditor/PbHtmlEditor.csproj
--- start.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "PB HTML Dev Server" pwsh -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\init-server-manager.ps1"
